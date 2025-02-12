3f769b1b502ca7678cf8bfa52337230f7f603e0b
okihara <okihara@gmail.com>
2025-02-13 02:13:53 +0900

    Herokuデプロイの準備
    - Procfileの追加
    - requirements.txtの更新
    - runtime.txtの追加
    - 環境変数対応の修正
    - .gitignoreの更新

--- app.py
+++ app.py
@@ -154,4 +154,5 @@ def video():
     return app.send_static_file('video.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
